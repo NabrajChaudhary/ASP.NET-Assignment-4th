06207b26587994594c2585bc85ccbe81fedcac7e
Rochak Rupakheti <rochak.rupakheti@study.lbef.edu.np>
2022-08-04 09:13:44 +0545

    New db created

--- BootstrapApp/login.aspx.cs
+++ BootstrapApp/login.aspx.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-namespace BootstrapApp
+﻿namespace BootstrapApp
 {
     public partial class Login : System.Web.UI.Page
     {
+        SqlConnection con = new sqlConnection('');
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
