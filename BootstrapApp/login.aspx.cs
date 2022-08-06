4d564c11bc761944ceb008175aaeedd9c7363254
NabrajChy1412 <nabrajchaudhary1412@gmail.com>
2022-08-07 05:39:37 +0545

    new pages

--- BootstrapApp/login.aspx.cs
+++ BootstrapApp/login.aspx.cs
@@ -2,9 +2,8 @@
 {
     public partial class Login : System.Web.UI.Page
     {
-        SqlConnection con = new sqlConnection('');
 
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender)
         {
 
         }
