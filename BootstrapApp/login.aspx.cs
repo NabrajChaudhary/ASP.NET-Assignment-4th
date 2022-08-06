b2a953b4e0e9167dcdaa9c8f51a5dbe42bbf9e50
NabrajChy1412 <nabrajchaudhary1412@gmail.com>
2022-08-06 21:54:46 +0545

    new elements on css

--- BootstrapApp/login.aspx.cs
+++ BootstrapApp/login.aspx.cs
@@ -1,8 +1,10 @@
-﻿namespace BootstrapApp
+﻿using System;
+
+namespace BootstrapApp
 {
     public partial class Login : System.Web.UI.Page
     {
-        SqlConnection con = new sqlConnection('');
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
