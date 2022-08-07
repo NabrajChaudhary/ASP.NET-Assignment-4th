17a7793c5ecd62a5faf06b04ac9f1133bea392e1
NabrajChy1412 <nabrajchaudhary1412@gmail.com>
2022-08-07 10:32:33 +0545

    new buttons adeded

--- BootstrapApp/login.aspx.designer.cs
+++ BootstrapApp/login.aspx.designer.cs
@@ -76,5 +76,23 @@ namespace BootstrapApp
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink hlForgotPasswords;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
